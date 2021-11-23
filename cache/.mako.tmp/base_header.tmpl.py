4e87e4620182f087ba3d2179fb48e16452490c76
Seth Garcia <frozencreed@gmail.com>
2021-11-23 10:03:16 -0900

    Cleaning up histories

---
+++ cache/.mako.tmp/base_header.tmpl.py
@@ -0,0 +1,198 @@
+# -*- coding:utf-8 -*-
+from mako import runtime, filters, cache
+UNDEFINED = runtime.UNDEFINED
+STOP_RENDERING = runtime.STOP_RENDERING
+__M_dict_builtin = dict
+__M_locals_builtin = locals
+_magic_number = 10
+_modified_time = 1637693637.2402155
+_enable_loop = True
+_template_filename = 'themes/canterville/templates/base_header.tmpl'
+_template_uri = 'base_header.tmpl'
+_source_encoding = 'utf-8'
+_exports = ['html_header', 'html_site_title', 'html_navigation_links', 'html_translation_header']
+
+
+
+from nikola.utils import slugify
+
+
+
+from nikola.utils import slugify
+
+
+
+from nikola.utils import slugify
+
+
+def _mako_get_namespace(context, name):
+    try:
+        return context.namespaces[(__name__, name)]
+    except KeyError:
+        _mako_generate_namespaces(context)
+        return context.namespaces[(__name__, name)]
+def _mako_generate_namespaces(context):
+    ns = runtime.TemplateNamespace('base', context._clean_inheritance_tokens(), templateuri='base_helper.tmpl', callables=None,  calling_uri=_template_uri)
+    context.namespaces[(__name__, 'base')] = ns
+
+def render_body(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        __M_locals = __M_dict_builtin(pageargs=pageargs)
+        _import_ns = {}
+        _mako_get_namespace(context, 'base')._populate(_import_ns, ['*'])
+        __M_writer = context.writer()
+        __M_writer('\r\n\r\n')
+        __M_writer('\r\n\r\n')
+        __M_writer('\r\n\r\n')
+        __M_writer('\r\n\r\n')
+        __M_writer('\r\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_html_header(context):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        _import_ns = {}
+        _mako_get_namespace(context, 'base')._populate(_import_ns, ['*'])
+        search_form = _import_ns.get('search_form', context.get('search_form', UNDEFINED))
+        def html_navigation_links():
+            return render_html_navigation_links(context)
+        def html_site_title():
+            return render_html_site_title(context)
+        template_hooks = _import_ns.get('template_hooks', context.get('template_hooks', UNDEFINED))
+        def html_translation_header():
+            return render_html_translation_header(context)
+        __M_writer = context.writer()
+        __M_writer('\r\n    <header id="header">\r\n        ')
+        __M_writer(str(html_site_title()))
+        __M_writer('\r\n        ')
+        __M_writer(str(html_translation_header()))
+        __M_writer('\r\n        ')
+        __M_writer(str(html_navigation_links()))
+        __M_writer('\r\n')
+        if search_form:
+            __M_writer('            <div class="searchform" role="search">\r\n                ')
+            __M_writer(str(search_form))
+            __M_writer('\r\n            </div>\r\n')
+        __M_writer('    </header>\r\n    ')
+        __M_writer(str(template_hooks['page_header']()))
+        __M_writer('\r\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_html_site_title(context):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        _import_ns = {}
+        _mako_get_namespace(context, 'base')._populate(_import_ns, ['*'])
+        blog_title = _import_ns.get('blog_title', context.get('blog_title', UNDEFINED))
+        logo_url = _import_ns.get('logo_url', context.get('logo_url', UNDEFINED))
+        _link = _import_ns.get('_link', context.get('_link', UNDEFINED))
+        abs_link = _import_ns.get('abs_link', context.get('abs_link', UNDEFINED))
+        show_blog_title = _import_ns.get('show_blog_title', context.get('show_blog_title', UNDEFINED))
+        lang = _import_ns.get('lang', context.get('lang', UNDEFINED))
+        __M_writer = context.writer()
+        __M_writer('\r\n    <h1 id="brand"><a href="')
+        __M_writer(str(abs_link(_link("root", None, lang))))
+        __M_writer('" title="')
+        __M_writer(filters.html_escape(str(blog_title)))
+        __M_writer('" rel="home">\r\n')
+        if logo_url:
+            __M_writer('        <img src="')
+            __M_writer(str(logo_url))
+            __M_writer('" alt="')
+            __M_writer(filters.html_escape(str(blog_title)))
+            __M_writer('" id="logo">\r\n')
+        __M_writer('\r\n')
+        if show_blog_title:
+            __M_writer('        <span id="blog-title">')
+            __M_writer(filters.html_escape(str(blog_title)))
+            __M_writer('</span>\r\n')
+        __M_writer('    </a></h1>\r\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_html_navigation_links(context):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        _import_ns = {}
+        _mako_get_namespace(context, 'base')._populate(_import_ns, ['*'])
+        permalink = _import_ns.get('permalink', context.get('permalink', UNDEFINED))
+        tuple = _import_ns.get('tuple', context.get('tuple', UNDEFINED))
+        isinstance = _import_ns.get('isinstance', context.get('isinstance', UNDEFINED))
+        lang = _import_ns.get('lang', context.get('lang', UNDEFINED))
+        template_hooks = _import_ns.get('template_hooks', context.get('template_hooks', UNDEFINED))
+        navigation_links = _import_ns.get('navigation_links', context.get('navigation_links', UNDEFINED))
+        rel_link = _import_ns.get('rel_link', context.get('rel_link', UNDEFINED))
+        __M_writer = context.writer()
+        __M_writer('\r\n<div class="nav">\r\n    <h3 class="nav-title">Menu</h3>\r\n    <a href="#" class="nav-close">\r\n        <span class="hidden">Close</span>\r\n    </a>\r\n    <ul>\r\n')
+        for url, text in navigation_links[lang]:
+            if isinstance(url, tuple):
+                __M_writer('            ')
+                __M_writer('\r\n\r\n            <li class="nav-opened" role="presentation">\r\n                <a href="javascript:void(0)" class="nested-url" data-children="nested-')
+                __M_writer(str(slugify(text, lang)))
+                __M_writer('">\r\n                ')
+                __M_writer(str(text))
+                __M_writer('\r\n                </a>\r\n            </li>\r\n')
+                for sublink, subtext in url:
+                    __M_writer('                <li class="nav-opened nested-')
+                    __M_writer(str(slugify(text, lang)))
+                    __M_writer(' hidden" role="presentation">\r\n                    <a href="')
+                    __M_writer(str(sublink))
+                    __M_writer('" class="child">\r\n                    ')
+                    __M_writer(str(subtext))
+                    __M_writer('\r\n                    </a>\r\n                </li>\r\n')
+            else:
+                if rel_link(permalink, url) == "#":
+                    __M_writer('            <li class="nav-opened nav-current" role="presentation">\r\n')
+                else:
+                    __M_writer('            <li class="nav-opened" role="presentation">\r\n')
+                __M_writer('                <a href="')
+                __M_writer(str(url))
+                __M_writer('">')
+                __M_writer(str(text))
+                __M_writer('</a>\r\n            </li>\r\n')
+        __M_writer('    ')
+        __M_writer(str(template_hooks['menu']()))
+        __M_writer('\r\n    ')
+        __M_writer(str(template_hooks['menu_alt']()))
+        __M_writer('\r\n    </ul>\r\n</div>\r\n<span class="nav-cover"></span>\r\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_html_translation_header(context):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        _import_ns = {}
+        _mako_get_namespace(context, 'base')._populate(_import_ns, ['*'])
+        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
+        base = _mako_get_namespace(context, 'base')
+        translations = _import_ns.get('translations', context.get('translations', UNDEFINED))
+        len = _import_ns.get('len', context.get('len', UNDEFINED))
+        __M_writer = context.writer()
+        __M_writer('\r\n')
+        if len(translations) > 1:
+            __M_writer('        <div id="toptranslations">\r\n            <h2>')
+            __M_writer(str(messages("Languages:")))
+            __M_writer('</h2>\r\n            ')
+            __M_writer(str(base.html_translations()))
+            __M_writer('\r\n        </div>\r\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+"""
+__M_BEGIN_METADATA
+{"filename": "themes/canterville/templates/base_header.tmpl", "uri": "base_header.tmpl", "source_encoding": "utf-8", "line_map": {"16": 39, "17": 40, "18": 41, "19": 42, "20": 39, "21": 40, "22": 41, "23": 42, "24": 39, "25": 40, "26": 41, "27": 42, "35": 2, "38": 0, "45": 2, "46": 16, "47": 28, "48": 70, "49": 79, "55": 4, "69": 4, "70": 6, "71": 6, "72": 7, "73": 7, "74": 8, "75": 8, "76": 9, "77": 10, "78": 11, "79": 11, "80": 14, "81": 15, "82": 15, "88": 18, "100": 18, "101": 19, "102": 19, "103": 19, "104": 19, "105": 20, "106": 21, "107": 21, "108": 21, "109": 21, "110": 21, "111": 23, "112": 24, "113": 25, "114": 25, "115": 25, "116": 27, "122": 30, "135": 30, "136": 37, "137": 38, "138": 39, "139": 41, "140": 44, "141": 44, "142": 45, "143": 45, "144": 48, "145": 49, "146": 49, "147": 49, "148": 50, "149": 50, "150": 51, "151": 51, "152": 55, "153": 56, "154": 57, "155": 58, "156": 59, "157": 61, "158": 61, "159": 61, "160": 61, "161": 61, "162": 65, "163": 65, "164": 65, "165": 66, "166": 66, "172": 72, "182": 72, "183": 73, "184": 74, "185": 75, "186": 75, "187": 76, "188": 76, "194": 188}}
+__M_END_METADATA
+"""
---
+++ cache/.mako.tmp/post.tmpl.py
@@ -0,0 +1,186 @@
+# -*- coding:utf-8 -*-
+from mako import runtime, filters, cache
+UNDEFINED = runtime.UNDEFINED
+STOP_RENDERING = runtime.STOP_RENDERING
+__M_dict_builtin = dict
+__M_locals_builtin = locals
+_magic_number = 10
+_modified_time = 1637693636.997755
+_enable_loop = True
+_template_filename = 'themes/canterville/templates/post.tmpl'
+_template_uri = 'post.tmpl'
+_source_encoding = 'utf-8'
+_exports = ['extra_head', 'content']
+
+
+def _mako_get_namespace(context, name):
+    try:
+        return context.namespaces[(__name__, name)]
+    except KeyError:
+        _mako_generate_namespaces(context)
+        return context.namespaces[(__name__, name)]
+def _mako_generate_namespaces(context):
+    ns = runtime.TemplateNamespace('helper', context._clean_inheritance_tokens(), templateuri='post_helper.tmpl', callables=None,  calling_uri=_template_uri)
+    context.namespaces[(__name__, 'helper')] = ns
+
+    ns = runtime.TemplateNamespace('pheader', context._clean_inheritance_tokens(), templateuri='post_header.tmpl', callables=None,  calling_uri=_template_uri)
+    context.namespaces[(__name__, 'pheader')] = ns
+
+    ns = runtime.TemplateNamespace('comments', context._clean_inheritance_tokens(), templateuri='comments_helper.tmpl', callables=None,  calling_uri=_template_uri)
+    context.namespaces[(__name__, 'comments')] = ns
+
+    ns = runtime.TemplateNamespace('math', context._clean_inheritance_tokens(), templateuri='math_helper.tmpl', callables=None,  calling_uri=_template_uri)
+    context.namespaces[(__name__, 'math')] = ns
+
+def _mako_inherit(template, context):
+    _mako_generate_namespaces(context)
+    return runtime._inherit_from(context, 'base.tmpl', _template_uri)
+def render_body(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        __M_locals = __M_dict_builtin(pageargs=pageargs)
+        def content():
+            return render_content(context._locals(__M_locals))
+        comments = _mako_get_namespace(context, 'comments')
+        date_format = context.get('date_format', UNDEFINED)
+        author_pages_generated = context.get('author_pages_generated', UNDEFINED)
+        site_has_comments = context.get('site_has_comments', UNDEFINED)
+        def extra_head():
+            return render_extra_head(context._locals(__M_locals))
+        post = context.get('post', UNDEFINED)
+        parent = context.get('parent', UNDEFINED)
+        _link = context.get('_link', UNDEFINED)
+        helper = _mako_get_namespace(context, 'helper')
+        math = _mako_get_namespace(context, 'math')
+        messages = context.get('messages', UNDEFINED)
+        __M_writer = context.writer()
+        __M_writer('\r\n')
+        __M_writer('\r\n')
+        __M_writer('\r\n')
+        __M_writer('\r\n')
+        __M_writer('\r\n\r\n')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'extra_head'):
+            context['self'].extra_head(**pageargs)
+        
+
+        __M_writer('\r\n\r\n')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
+            context['self'].content(**pageargs)
+        
+
+        __M_writer('\r\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_extra_head(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def extra_head():
+            return render_extra_head(context)
+        parent = context.get('parent', UNDEFINED)
+        post = context.get('post', UNDEFINED)
+        helper = _mako_get_namespace(context, 'helper')
+        math = _mako_get_namespace(context, 'math')
+        __M_writer = context.writer()
+        __M_writer('\r\n    ')
+        __M_writer(str(parent.extra_head()))
+        __M_writer('\r\n')
+        if post.meta('keywords'):
+            __M_writer('    <meta name="keywords" content="')
+            __M_writer(filters.html_escape(str(post.meta('keywords'))))
+            __M_writer('">\r\n')
+        __M_writer('    <meta name="author" content="')
+        __M_writer(filters.html_escape(str(post.author())))
+        __M_writer('">\r\n')
+        if post.prev_post:
+            __M_writer('        <link rel="prev" href="')
+            __M_writer(str(post.prev_post.permalink()))
+            __M_writer('" title="')
+            __M_writer(filters.html_escape(str(post.prev_post.title())))
+            __M_writer('" type="text/html">\r\n')
+        if post.next_post:
+            __M_writer('        <link rel="next" href="')
+            __M_writer(str(post.next_post.permalink()))
+            __M_writer('" title="')
+            __M_writer(filters.html_escape(str(post.next_post.title())))
+            __M_writer('" type="text/html">\r\n')
+        if post.is_draft:
+            __M_writer('        <meta name="robots" content="noindex">\r\n')
+        __M_writer('    ')
+        __M_writer(str(helper.open_graph_metadata(post)))
+        __M_writer('\r\n    ')
+        __M_writer(str(helper.twitter_card_information(post)))
+        __M_writer('\r\n    ')
+        __M_writer(str(helper.meta_translations(post)))
+        __M_writer('\r\n    ')
+        __M_writer(str(math.math_styles_ifpost(post)))
+        __M_writer('\r\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_content(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def content():
+            return render_content(context)
+        comments = _mako_get_namespace(context, 'comments')
+        date_format = context.get('date_format', UNDEFINED)
+        author_pages_generated = context.get('author_pages_generated', UNDEFINED)
+        site_has_comments = context.get('site_has_comments', UNDEFINED)
+        _link = context.get('_link', UNDEFINED)
+        post = context.get('post', UNDEFINED)
+        math = _mako_get_namespace(context, 'math')
+        messages = context.get('messages', UNDEFINED)
+        __M_writer = context.writer()
+        __M_writer('\r\n\r\n  <article class="post post">\r\n    <header class="post-header">\r\n        <h1 class="post-title">')
+        __M_writer(str(post.title()))
+        __M_writer('</h1>\r\n        <section class="post-meta"> by\r\n')
+        if author_pages_generated:
+            __M_writer('            <a href="')
+            __M_writer(str(_link('author', post.author())))
+            __M_writer('">')
+            __M_writer(filters.html_escape(str(post.author())))
+            __M_writer('</a>\r\n')
+        else:
+            __M_writer('            ')
+            __M_writer(filters.html_escape(str(post.author())))
+            __M_writer('\r\n')
+        __M_writer('            on\r\n')
+        for tag in post.tags:
+            __M_writer('                <a href="link://tag/')
+            __M_writer(str(tag))
+            __M_writer('">#')
+            __M_writer(str(tag))
+            __M_writer('</a>,\r\n')
+        __M_writer('            <time class="post-date" datetime="')
+        __M_writer(str(post.formatted_date('webiso')))
+        __M_writer('">\r\n                ')
+        __M_writer(filters.html_escape(str(post.formatted_date(date_format))))
+        __M_writer('\r\n            </time>\r\n        </section>\r\n    </header>\r\n\r\n    <section class="post-content">\r\n    ')
+        __M_writer(str(post.text()))
+        __M_writer('\r\n    </section>\r\n    <footer class="post-footer">\r\n')
+        if not post.meta('nocomments') and site_has_comments:
+            __M_writer('        <section class="comments hidden-print">\r\n        <h2>')
+            __M_writer(str(messages("Comments")))
+            __M_writer('</h2>\r\n        ')
+            __M_writer(str(comments.comment_form(post.permalink(absolute=True), post.title(), post._base_path)))
+            __M_writer('\r\n        </section>\r\n')
+        __M_writer('    ')
+        __M_writer(str(math.math_scripts_ifpost(post)))
+        __M_writer('\r\n</article>\r\n')
+        __M_writer(str(comments.comment_link_script()))
+        __M_writer('\r\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+"""
+__M_BEGIN_METADATA
+{"filename": "themes/canterville/templates/post.tmpl", "uri": "post.tmpl", "source_encoding": "utf-8", "line_map": {"23": 2, "26": 3, "29": 4, "32": 5, "38": 0, "57": 2, "58": 3, "59": 4, "60": 5, "61": 6, "66": 27, "71": 63, "77": 8, "87": 8, "88": 9, "89": 9, "90": 10, "91": 11, "92": 11, "93": 11, "94": 13, "95": 13, "96": 13, "97": 14, "98": 15, "99": 15, "100": 15, "101": 15, "102": 15, "103": 17, "104": 18, "105": 18, "106": 18, "107": 18, "108": 18, "109": 20, "110": 21, "111": 23, "112": 23, "113": 23, "114": 24, "115": 24, "116": 25, "117": 25, "118": 26, "119": 26, "125": 29, "139": 29, "140": 33, "141": 33, "142": 35, "143": 36, "144": 36, "145": 36, "146": 36, "147": 36, "148": 37, "149": 38, "150": 38, "151": 38, "152": 40, "153": 41, "154": 42, "155": 42, "156": 42, "157": 42, "158": 42, "159": 44, "160": 44, "161": 44, "162": 45, "163": 45, "164": 51, "165": 51, "166": 54, "167": 55, "168": 56, "169": 56, "170": 57, "171": 57, "172": 60, "173": 60, "174": 60, "175": 62, "176": 62, "182": 176}}
+__M_END_METADATA
+"""
