93f9923043a367e467af170666144a18be22e9e6
Seth Garcia <frozencreed@gmail.com>
2021-11-23 18:08:11 -0900

    fixed ghformat script

--- ghformat.py
+++ ghformat.py
@@ -1,8 +1,4 @@
 import os
-import re
-import sys
-import traceback
-import collections
 import shutil
 
 pagedir = r"C:\Users\froze\OneDrive\Documents\Python\Websites\KGA\output\pages"
@@ -11,13 +7,13 @@ outputdir = r"C:\Users\froze\OneDrive\Documents\Python\Websites\KGA\output"
 
 for root, dirs, files in os.walk(pagedir):
     for filename in dirs:
-        if filename == 'index.html':
-            old_name = os.path.join( os.path.abspath(root), filename )
+        if filename == r"index.html":
+            old_name = os.path.join(os.path.abspath(root), filename)
             base, extension = os.path.splitext(filename)
             new_name = os.path.join(basedir, base, dirs)
 
             shutil.copy(old_name, new_name)
 
-homepage = outputdir + '\index.html'
-githubhomepage = basedir + '\index.html'
+homepage = outputdir + r"\index.html"
+githubhomepage = basedir + r"\index.html"
 shutil.copy(homepage, githubhomepage)
